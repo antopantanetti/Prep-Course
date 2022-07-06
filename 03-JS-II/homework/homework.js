08ef23c8efbc6708170f43c7e6149f56c2472cc7
antonella <antonellapantanetti@gmail.com>
2022-07-06 17:10:02 -0300

    terminado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -19,9 +19,9 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if (edad >= 18){
-    return "allowed";
+    return "Allowed";
   } else {
-    return "not allowed";
+    return "Not allowed";
   }
 }
   
@@ -47,11 +47,11 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if (idioma === aleman){
+  if (idioma === "aleman"){
     return "Guten Tag!";
-  } else if (idioma === mandarin){
+  } else if (idioma === "mandarin"){
     return "Ni Hao!";
-  } else if (idioma === ingles){
+  } else if (idioma === "ingles"){
     return "Hello!";
   } else {
     return "Hola!";
@@ -76,7 +76,7 @@ function colors(color) {
     case "orange":
       return "This is orange";
     default:
-      return "Colour not found";
+      return "Color not found";
   }
 }
 
@@ -87,7 +87,7 @@ function esDiezOCinco(numero) {
   if (numero === 10 || numero === 5){
     return true;
   } else {
-    return "false";
+    return false;
   }
 }
 
@@ -122,13 +122,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3){
-    return "fizz"
-  } else if (numero % 5){
-    return "buzz";
-  } else (numero % 3 && numero % 5);{
-    return "fizzbuzz";
-  }
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -138,15 +135,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > 3 && num1 > 0){
-    return "Número 1 es mayor y positivo";
-  } else if (num1 < 0 || num2 < 0 || num3 < 0){
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
-  } else if (num3 > num1 && num3 > num2){
-    return num3 + 1;
-  } else if (num1 === 0 || num2 === 0 || num3 === 0){
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
     return "Error";
-  } else {
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
     return false;
   }
 }
@@ -157,14 +158,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  for (var i = 2; i <= numero; i++){
-    if ((numero % 1) === 0){
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++){
+    if(numero % i === 0) {
       return false;
-    } else {
-      return true;
     }
-  }
+ } return true;
 }
+
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
